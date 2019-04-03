5c187202de10826eaf1022eed8c3f6db479e3723
samytichadou <samytichadou@gmail.com>
2019-04-03 21:22:02 +0200

    naming

--- __init__.py
+++ __init__.py
@@ -63,10 +63,10 @@ def register():
 
     bpy.types.Scene.playblaster_render_engine = \
         bpy.props.EnumProperty(
-                        name = "Render Engine",
+                        name = "Engine",
                         default = 'BLENDER_WORKBENCH',
                         items = (
-                        ('BLENDER_WORKBENCH', "Workbench Engine", ""),
+                        ('BLENDER_WORKBENCH', "Workbench", ""),
                         ('BLENDER_EEVEE', "EEVEE", ""),
                         ))
 
